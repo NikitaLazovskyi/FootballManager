3bba4ab3f2de389f6b458b1035f28ec849a43fb3
Nikita <junior.lazovskyi@gmail.com>
2022-05-18 16:09:35 +0300

    init data source on start up

---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+insert into team (bank_account, commission, name)
+values (2000000, 8, 'FC Manchester City');
+insert into team (bank_account, commission, name)
+values (3000000, 7, 'FC Barcelona');
+insert into team (bank_account, commission, name)
+values (2500000, 6, 'FC Chelsea');
+-- insert into team (bank_account, commission, name)
+-- values (100000, 8, 'FC Bavaria');
+
+insert into player (age, experience, lastname, name, team_id, cost) VALUES (35, 170, 'Andriy', 'Shevchenko', 1, 170 * 100000 / 35);
+insert into player (age, experience, lastname, name, team_id, cost) VALUES (25, 45, 'Lionel', 'Messi', 1, 45 * 100000 / 25);
+insert into player (age, experience, lastname, name, team_id, cost) VALUES (23, 30, 'Huri', 'Cane', 2, 30 * 100000 / 23);
+insert into player (age, experience, lastname, name, team_id, cost) VALUES (27, 70, 'Cristiano', 'Ronaldo', 2, 70 * 100000 / 27);
+insert into player (age, experience, lastname, name, team_id, cost) VALUES (20, 0, 'Lil', 'Wayne', 3, 0 * 100000 / 20);
+insert into player (age, experience, lastname, name, team_id, cost) VALUES (21, 12, 'Pogbe', 'Pogbovich', 3, 12 * 100000 / 21);
+insert into player (age, experience, lastname, name, team_id, cost) VALUES (40, 240, 'Puel', 'Puelevich', 3, 240 * 100000 / 40);
