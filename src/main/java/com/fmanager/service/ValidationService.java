4cc3a0ba79d9f4fcc6727581210af2c1db559c99
Nikita <junior.lazovskyi@gmail.com>
2022-05-29 01:53:30 +0300

    improving filtering

--- src/main/java/com/fmanager/service/ValidationService.java
+++ src/main/java/com/fmanager/service/ValidationService.java
@@ -6,21 +6,27 @@ import com.fmanager.entity.Team;
 import java.math.BigDecimal;
 
 public class ValidationService {
-    private static final String pName = "^[-\\w\\s]+$";
+    private static final String pName = "^[a-zA-Z\\s-]{2,}$";
 
     public static boolean validate(Player player){
-        return player.getAge() >= 18
+        return player.getName() != null
+                && player.getLastname() != null
+                && player.getExperience() != null
+                && player.getAge() != null
+                && player.getTeam() != null
+                && player.getAge() >= 18
                 && player.getExperience() >= 0
-                && player.getName() != null
                 && player.getName().matches(pName)
                 && player.getLastname().matches(pName);
     }
 
     public static boolean validate(Team team){
-        return team.getBankAccount().compareTo(new BigDecimal(0)) >= 0
+        return team.getCommission() != null
+                && team.getName() != null
+                && team.getBankAccount() != null
+                && team.getBankAccount().compareTo(new BigDecimal(0)) >= 0
                 && team.getCommission() >= 0
                 && team.getCommission() <= 10
-                && team.getName() != null
                 && team.getName().matches(pName);
     }
 }
