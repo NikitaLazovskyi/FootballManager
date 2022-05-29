d649301266a02404879659d05cf6945c7e0fba31
Nikita <junior.lazovskyi@gmail.com>
2022-05-29 05:03:56 +0300

    every 2 week checks cost of all players

--- src/main/java/com/fmanager/commands/UpdateCostCommand.java
+++ src/main/java/com/fmanager/commands/UpdateCostCommand.java
@@ -4,11 +4,14 @@ import com.fmanager.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Order(value = 1)
+@EnableScheduling
 @Component
-public class UpdateCostCommand implements CommandLineRunner {
+public class UpdateCostCommand{
 
     private final PlayerService playerService;
 
@@ -17,8 +20,11 @@ public class UpdateCostCommand implements CommandLineRunner {
         this.playerService = playerService;
     }
 
-    @Override
-    public void run(String... args) throws Exception {
+    /**
+     * One time per two week will check cost
+     */
+    @Scheduled(fixedRate = 1000/*millis*/ * 60/*sek*/ * 60/*min*/ * 24/*hours*/ * 14/*days*/)
+    public void run(){
         playerService.updateCostOfEachPlayer();
     }
 }
